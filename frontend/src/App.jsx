c0d4e23be30eca882a4b7317326ae8f02f8afef3
fawzisilver <90504301+fawzisilver@users.noreply.github.com>
2024-08-20 17:40:55 -0600

    added custom boostrap, finished designing Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container} from 'react-bootstrap'
 
 const App = () => {
   return (
     <>
-    <h1>Welcome to Pro Shop</h1>
-    <Header />
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to Tech Shop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
