6c93eaf271a8831e8ee404fc2c7c26980bbe8d2f
fawzisilver <90504301+fawzisilver@users.noreply.github.com>
2024-08-26 16:14:03 -0600

    added .env file and nodemon with concurrently to run frontend and backend simulatenously

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import products from './data/products.js';
+import dotenv from 'dotenv'
+dotenv.config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     res.send('API is running...')
