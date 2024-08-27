3e4e2376254553c7c787848d83d374794c30295e
fawzisilver <90504301+fawzisilver@users.noreply.github.com>
2024-08-26 21:37:14 -0600

    installed cors to allow cross-origin and fix bug for fetching data from backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import products from './data/products.js';
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config();
+
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send('API is running...')
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,8 +1,39 @@
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import products from '../products'
+// import products from '../products'
+import axios from 'axios'
+import { useEffect, useState } from 'react';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+ 
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/products');
+        
+        // Check if the response is OK (status 200-299)
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        
+        // Parse the JSON data
+        const data = await response.json();
+        
+        // Update the products state
+        setProducts(data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+  
+    fetchProducts();
+  }, []);
+  
+  
+  
+
+
   return (
     <>
         <h1>Latest Products</h1>
@@ -18,3 +49,9 @@ const HomeScreen = () => {
 }
 
 export default HomeScreen
+
+
+
+
+ 
+
