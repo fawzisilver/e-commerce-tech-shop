dc411c06a22009ad293aaf6b4c285de09541984e
fawzisilver <90504301+fawzisilver@users.noreply.github.com>
2024-08-29 03:09:23 -0600

    setup mongoDB atlas and our mongoose... need the database content now

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,10 @@ import express from 'express'
 import products from './data/products.js';
 import dotenv from 'dotenv'
 import cors from 'cors'
+import connectDB from './config/db.js';
 dotenv.config();
 
-
+connectDB(); // connect to mongoDB
 const app = express();
 const port = process.env.PORT || 5000;
 
