bfa575b8a4bb5fe966dd48b6e9d5a303ca7d841c
fawzisilver <90504301+fawzisilver@users.noreply.github.com>
2024-09-08 19:31:41 -0600

    fix minor bugs regarding redux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Outlet } from 'react-router-dom'
 import { Container} from 'react-bootstrap'
-import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
--- frontend/store.js
+++ frontend/store.js
@@ -1,8 +1,13 @@
 //entry point of redux
 import { configureStore } from '@reduxjs/toolkit'
+import { apiSlice } from './src/slices/apiSlice';
 
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true,
 });
 
 export default store;
