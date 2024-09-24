04535bc75a6a90c141ddc589631855b4e30fd8bb
fawzisilver <90504301+fawzisilver@users.noreply.github.com>
2024-09-23 19:22:18 -0600

    part 1 authentication: imported all necessary tools and created design the loginScreen

---
+++ frontend/src/screens/LoginScreen.jsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from "react-redux"; //dispatch action and access global state like user
+import Loader from "../components/Loader"; //when its loading
+import FormContainer from '../components/FormContainer';
+import { useLoginMutation } from "../slices/usersApiSlice"; //for apis (post method)
+import { setCredentials } from "../slices/authSlice"; //a action creator from reducer
+import { toast } from 'react-toastify'
+
+const LoginScreen = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [login, { isLoading }] = useLoginMutation(); //isLoading and error is created by useLoginMutation
+
+    const { userInfo } = useSelector((state) => state.auth) //auth is the slice (has all info)
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log('submit');
+        
+    }
+
+  return (
+    <FormContainer>
+        <h1>Sign In</h1>
+
+        <Form onSubmit={submitHandler}>
+            <Form.Group controlId="email" className="my-3">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    value={email}
+                    onChange={(e)=> setEmail(e.target.value)}
+                >
+                </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="password" className="my-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    type="password"
+                    placeholder="Enter password"
+                    value={password}
+                    onChange={(e)=> setPassword(e.target.value)}
+                >
+                </Form.Control>
+            </Form.Group>
+
+            <Button type="submit" variant='primary' className="mt-2">
+                Sign In
+            </Button>
+        </Form>
+
+            <Row className="py-3">
+                <Col>
+                    New Customer? <Link to="/register">Register</Link>
+                </Col>
+            </Row>
+
+    </FormContainer>
+        )
+}
+
+export default LoginScreen
--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -4,9 +4,9 @@ import { apiSlice } from "./apiSlice"; //parent to usersApiSlice
 // defines user related API endpoints
 export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
-        login: builder.mutation({
+        login: builder.mutation({ //mutation is for action like creating, updating, or deleting
             query: (data) => ({
-                url: USERS_URL / auth, //`${USERS_URL}/auth`
+                url: `${USERS_URL}/auth`, //`${USERS_URL}/auth`
                 method: 'POST',
                 body: 'data',
             }),
