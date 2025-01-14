00448faa56fbce10cba7969f49d5e1ce1cb72d98
fawzisilver <isaacabelida@gmail.com>
2025-01-14 10:31:07 -0700

    hot fixing server

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,16 @@
-import path from 'path';
-import express from 'express'
-import products from './data/products.js';
-import dotenv from 'dotenv'
-import cors from 'cors'
-import connectDB from './config/db.js';
-import cookieParser from 'cookie-parser';
+import path from "path";
+import express from "express";
+import products from "./data/products.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
-import productRoutes from "./routes/productRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
-import orderRoutes from './routes/orderRoutes.js'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import uploadRoutes from './routes/uploadRoutes.js';
-
-
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 connectDB(); // connect to mongoDB
 const port = process.env.PORT || 5000;
@@ -21,45 +19,49 @@ const app = express();
 
 // Body parser middleware (lets us extract req.body)
 app.use(express.json()); //raw json
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Cookie parser middleware
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use(cors({
-    origin: 'http://localhost:5173', // The URL of your frontend
+app.use(
+  cors({
+    origin: "http://localhost:5173", // The URL of your frontend
     credentials: true, // Allows sending cookies
-}));
+  })
+);
 //previous app.use(cors());
 
-app.use('/api/products', productRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/orders', orderRoutes)
-app.use('/api/upload', uploadRoutes) 
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
 const __dirname = path.resolve(); // Set __dirname to current dir
-app.use('/images', express.static(path.join(__dirname, 'frontend/public/images')));
+app.use(
+  "/images",
+  express.static(path.join(__dirname, "frontend/public/images"))
+);
 
+app.get("/api/config/paypal", (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
 
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }))
-
-if (process.env.NODE_ENV === 'production') {
-    // set static folder
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
-
-    app.get('*', (req, res) => 
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-    );
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "frontend/dist", "index.html"))
+  );
+  //before change
+  // app.get('*', (req, res) =>
+  //     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  // );
 }
-
 
 //overriding express error handler
 app.use(notFound);
 app.use(errorHandler);
 
-
-app.listen(port, ()=> console.log(`server is running on port ${port}`))
-
-
-
+app.listen(port, () => console.log(`server is running on port ${port}`));
